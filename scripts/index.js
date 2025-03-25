e75111d674c50088674b7e02a24d1e629d61b6ef
ruben <rubenhm04@usal.es>
2025-03-25 12:11:05 +0100

    Actualización bastante grande. Se ha añadidola interfaz para la red social. También se incluyen varias imagenes

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,14 @@ document.getElementById("cerrarOutfit").addEventListener("click", function() {
     var overlay = document.getElementById("overlay");
     overlay.style.display = "none"; // Oculta el fondo y la imagen
 });
+
+
+// Barra de buscar usuario
+document.getElementById("searchButton").addEventListener("click", function () {
+    let query = document.getElementById("searchInput").value.trim();
+    if (query !== "") {
+        setTimeout(() => {
+            window.open(`https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`, "_blank");
+        }, 500); // Simula recarga con un pequeño retraso
+    }
+});
